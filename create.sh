1179cdb42428421c8a47818deea457fba3fb7cf6
Simon Brundin <simonbrundin@gmail.com>
2022-09-22 19:30:17 +0200

    Uppdateringar

--- create.sh
+++ create.sh
@@ -7,3 +7,26 @@ cd nuxt-app
 mv * ../ && mv .* ../
 cd ..
 rm -r nuxt-app
+
+# Installera Pinia
+npm install @pinia/nuxt
+
+# Installera GraphQL Client
+npm install nuxt-graphql-client
+
+# Installera Capacitor och Ionic
+npm install nuxt-ionic -D
+npm install @capacitor/ios
+npm run build
+npx @ionic/cli integrations enable capacitor
+npx cap add ios 
+
+# Installera Vitest
+npm install -D vitest
+
+
+
+
+
+
+# Kopiera NuxtConfig
